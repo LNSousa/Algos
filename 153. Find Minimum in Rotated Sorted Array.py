034e5a6d43461128231c54a94fc28bc5b4156b14
Lucas Sousa <lucasns241@gmail.com>
2023-08-10 17:42:32 -0400

    08-10

---
+++ 153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,41 @@
+'''
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. 
+
+For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+- [4,5,6,7,0,1,2] if it was rotated 4 times.
+- [0,1,2,4,5,6,7] if it was rotated 7 times.
+
+Notice that rotating an array [a[0], a[1], a[2], ..., 
+a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, 
+return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+'''
+import math
+from typing import List
+
+def findMin(nums: List[int]) -> int:
+    minimum, max = 0, len(nums)-1
+    result = nums[minimum]
+
+    while minimum <= max:
+        if nums[minimum] < nums[max]:
+            result = min(result, nums[minimum])
+            break
+
+        i = (minimum + max) // 2
+        result = min(result, nums[i])
+
+        if nums[i] >= nums[minimum]:
+            minimum = i+1
+        else:
+            max = i-1
+
+    return result
+
+print(findMin([3,4,5,1,2]))
+print(findMin([4,5,6,7,0,1,2]))
+print(findMin([11,13,15,17]))
